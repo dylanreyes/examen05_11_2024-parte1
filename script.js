c48bc16ca59bc5f0d97e4f92bab40e758f4bbb07
Dyaln Reyes <dylanreyessegura@gmail.com>
2024-11-05 10:23:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function calcular() {
+    const horas = document.getElementById("horas").value;
+    const resultado = document.getElementById("resultado");
+
+
+    // Aquí puedes agregar la lógica para realizar el cálculo con las horas ingresadas
+    if (horas.textContent = 25 ){
+        resultado.textContent = "Tiene 1 ECTS";
+    };
+    if (horas.textContent = 50){
+        resultado.textContent = "Tiene 2 ECTS";
+    };
+
+}
